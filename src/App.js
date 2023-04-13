8a9e6258d441a7c1ecb3c017771a06072cae2f5a
Lewis Brennan <lewisbrennan444@gmail.com>
2023-04-13 11:39:07 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import BeerContainer from './containers/BeerContainer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+<h1>Brewdog is overrated</h1>
+<BeerContainer/>
+</div>
   );
 }
 
---
+++ src/containers/BeerContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const BeerContainer = () => {
+
+  return (
+    <h2>BeerContainer</h2>
+  )
+}
+
+export default BeerContainer
